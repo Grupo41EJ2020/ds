57591dbb81501693837c960f6e95e18bebb0d5cb
Jose Alejandro Ibarra Miranda <alexibarra130999@gmail.com>
2020-05-25 22:11:03 -0500

    Avance funcion mostrar

--- Controllers/TemaController.cs
+++ Controllers/TemaController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVCLaboratorio.Utilerias;
+using MVCLaboratorio.Models;
 
 namespace MVCLaboratorio.Controllers
 {
@@ -13,7 +17,20 @@ namespace MVCLaboratorio.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            DataTable dtTema = BaseHelper.ejecutarConsulta("sp_Tema_ConsultarTodo", CommandType.StoredProcedure);
+
+            List<Tema> lstTema = new List<Tema>();
+
+            foreach (DataRow item in dtTema.Rows)
+            {
+                Tema datosTema = new Tema();
+                datosTema.idTema = int.Parse(item["idTema"].ToString());
+                datosTema.Nombre = item["Nombre"].ToString();
+
+                lstTema.Add(datosTema);
+
+            }
+                return View();
         }
 
         //
